10f2edcb0adf49265dc2d4a3459ecd5ba3971a26
huro0906 <huro0906@gmail.com>
2025-04-12 16:10:15 +0900

    add: Google Search Console 인증 html추가

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744441644',resources: [
+    cacheName: 'chirpy-1744441804',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
