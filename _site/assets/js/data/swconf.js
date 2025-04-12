f6373ce490eba82aaaa227798660ef3866e80a29
huro0906 <huro0906@gmail.com>
2025-04-12 15:02:06 +0900

    init

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744437023',resources: [
+    cacheName: 'chirpy-1744437705',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
